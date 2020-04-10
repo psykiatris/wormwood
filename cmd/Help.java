485edd0217eb01070f918619d73f4cff1769460a
Patrick Palczewski <psykiatris@gmail.com>
2020-04-09 17:48:18 -0700

    Changed catch to manage all catchable exceptions into a collapsed method for space.

--- cmd/Help.java
+++ cmd/Help.java
@@ -1,12 +1,13 @@
 package cmd;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.regex.Pattern;
 import iface.Command;
 import obj.Player;
 import core.Output;
 import core.HandleData;
 
-/*
+/**
  *Help 
  *Prints a message describing the command given to it. 
  *If used without arguments, prints a list of all the game commands.";
@@ -21,24 +22,27 @@ public class Help implements Command {
             Class<?> cmdClass = Class.forName("cmd." + cmdName);
             Object obj = cmdClass.getConstructor().newInstance();
             Output.println(obj);
-         } catch (Exception classNotFoundException) {
-            Output.println(cmdName + " does not exist.");  
+         } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchMethodException | SecurityException | InvocationTargetException e) {
+            Output.println(cmdName + " does not exist.");
+         } catch (RuntimeException ignored) {
+            // Do nothing
          }
       }
 
       // Help not supplied with parameters, print a list of commands
       else {
-         Output.println("Game Commands: "); 
+         Output.println("Game Commands: ");
+
 
          // Get a list of filenames in the command directory
-         String[] cmdList = cmdDir.list();   
-         for (int i = 0; i < cmdList.length; i++) 
-            // Command names will be .java files
-            if (Pattern.matches(".*java$", cmdList[i])) {
+         String[] cmdList = cmdDir.list();
+         // Command names will be .java or .class files
+         for (String s : cmdList)
+            if ((Pattern.matches(".*java$", s)) || (Pattern.matches(".*class$", s))) {
                // Remove the .java from the command name
-               String cmd = cmdList[i].substring(0, cmdList[i].indexOf("."));
+               String cmd = s.substring(0, s.indexOf("."));
                Output.println(cmd);
-            }
+            } else if (cmdList.e)
 
          Output.println("Type help <cmd_name> for help on a specific command.");
       }
